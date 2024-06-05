8ac1b5a4a2cfbd1bcd86c4f53cc49471e70f208c
TechMukul <133253293+TechMukul@users.noreply.github.com>
2024-06-05 11:53:49 +0530

    nw

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3002'], // Add your production frontend URL here
+    origin: ['http://localhost:3001', 'http://localhost:3002', 'https://www.woxnpackagingsolution.com'], // Add your production frontend URL here
     credentials: true, // Enable credentials (if needed)
   });
   
